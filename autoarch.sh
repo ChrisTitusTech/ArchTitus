a9d90bec1a816cdab977663d244744693dcdeba2
Sajay <78424701+sajayprakash@users.noreply.github.com>
2021-10-24 11:16:07 +0530

    Update autoarch.sh

--- autoarch.sh
+++ autoarch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
     bash 0-preinstall.sh
-    arch-chroot /mnt /root/autoarch/1-setup.sh
-    source /mnt/root/autoarch/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/autoarch/2-user.sh
-    arch-chroot /mnt /root/autoarch/3-post-setup.sh
+    arch-chroot /mnt /root/AutoArch/1-setup.sh
+    source /mnt/root/AutoArch/install.conf
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/AutoArch/2-user.sh
+    arch-chroot /mnt /root/AutoArch/3-post-setup.sh
