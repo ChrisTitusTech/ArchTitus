f2f1ce7730d9c3708cae30b598f87dc0eef90fe3
Chris Titus <dfm.titus@gmail.com>
2021-10-26 17:33:34 -0500

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -34,7 +34,7 @@ sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
 timedatectl --no-ask-password set-timezone America/Chicago
 timedatectl --no-ask-password set-ntp 1
-localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_COLLATE="" LC_TIME="en_US.UTF-8"
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
 
 # Set keymaps
 localectl --no-ask-password set-keymap us
