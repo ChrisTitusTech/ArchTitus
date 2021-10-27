1ddf1a503c20ea56960f62ed46e0ba5500c1dd76
Sereinixej <62066769+Sereinixej@users.noreply.github.com>
2021-10-27 12:16:18 +0200

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -34,7 +34,7 @@ echo "       Setup Language to US and set locale       "
 echo "-------------------------------------------------"
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-timedatectl --no-ask-password set-timezone America/Chicago
+timedatectl --no-ask-password set-timezone Europe/Vienna
 timedatectl --no-ask-password set-ntp 1
 localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
 
