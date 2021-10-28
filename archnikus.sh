da91152e7398e24bdbb7f27e95e42b1a6fd9b8da
Nicholas <nicholas.osprey@outlook.com>
2021-10-27 23:59:29 -0700

    Errored out - Testing Dynamic filename
    Need to find way to get the name of the file containing all scripts without getting '.' when in the that directory, perhaps just another if statement

--- archnikus.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-PROJECTNAME="ArchNikus"
-
-    bash 0-preinstall.sh
-    arch-chroot /mnt /root/$PROJECTNAME/1-setup.sh
-    source /mnt/root/$PROJECTNAME/install.conf
-    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/$PROJECTNAME/2-user.sh
-    arch-chroot /mnt /root/$PROJECTNAME/3-post-setup.sh
---
+++ archtitus.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Find the name of the folder the scripts are in
+
+# X! Still erroring out, doesnt get the directory no matter what.
+
+if [ -L $0 ] ; then
+    ME=$(readlink $0)
+else
+    ME=$0
+fi
+dir=$(dirname $ME)
+
+export SCRIPTHOME="$(basename -- $dir)"
+echo "Scripts are in dir named $SCRIPTHOME"
+
+    bash 0-preinstall.sh
+    arch-chroot /mnt /root/$SCRIPTHOME/1-setup.sh
+    source /mnt/root/$SCRIPTHOME/install.conf
+    arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/$SCRIPTHOME/2-user.sh
+    arch-chroot /mnt /root/$SCRIPTHOME/3-post-setup.sh
+
+# Replace in the same state
+cd $PWD
+echo "
+###############################################################################
+# Done - Please Eject Install Media and Reboot (you can just type 'reboot')
+###############################################################################
+"
