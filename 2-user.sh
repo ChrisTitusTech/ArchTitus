ad30011ca54dd6867ea2ccb3656c58db33b2d8d7
Ahmed Al Balochi <a7mad98.work@gmail.com>
2021-11-03 00:58:43 +0500

    made seperate files for pacman and aur pkgs

--- 2-user.sh
+++ 2-user.sh
@@ -22,38 +22,7 @@ git clone "https://github.com/ChrisTitusTech/zsh"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
 ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 
-PKGS=(
-'autojump'
-'awesome-terminal-fonts'
-'brave-bin' # Brave Browser
-'dxvk-bin' # DXVK DirectX to Vulcan
-'github-desktop-bin' # Github Desktop sync
-'lightly-git'
-'lightlyshaders-git'
-'mangohud' # Gaming FPS Counter
-'mangohud-common'
-'nerd-fonts-fira-code'
-'nordic-darker-standard-buttons-theme'
-'nordic-darker-theme'
-'nordic-kde-git'
-'nordic-theme'
-'noto-fonts-emoji'
-'papirus-icon-theme'
-'plasma-pa'
-'ocs-url' # install packages from websites
-'sddm-nordic-theme-git'
-'snapper-gui-git'
-'ttf-droid'
-'ttf-hack'
-'ttf-meslo' # Nerdfont package
-'ttf-roboto'
-'zoom' # video conferences
-'snap-pac'
-)
-
-for PKG in "${PKGS[@]}"; do
-    yay -S --noconfirm $PKG
-done
+yay -S --needed - < /pkg-files/aur-pkgs.txt
 
 export PATH=$PATH:~/.local/bin
 cp -r $HOME/ArchTitus/dotfiles/* $HOME/.config/
