1b495e1d9c9712b1bac002565b5961e906c441cc
Oscar M. Cantu <omcantu@gmail.com>
2021-11-16 20:54:47 -0600

    Update archtitus.sh
    removing arch base install

--- archtitus.sh
+++ archtitus.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-    bash 0-preinstall.sh
     arch-chroot /mnt /root/ArchTitus/1-setup.sh
     source /mnt/root/ArchTitus/install.conf
     arch-chroot /mnt /usr/bin/runuser -u $username -- /home/$username/ArchTitus/2-user.sh
