147c6773e20d1cc04cd2a019b2348a3ea6e34414
Pavel Allahverdov <95222382+x-crowbar-x@users.noreply.github.com>
2021-12-04 17:04:08 +0300

    Add CyberRe Grub theme install
    Took some code from the 'install.sh' (https://www.gnome-look.org/p/1420727/). 'CyberRe' theme directory should be now uploaded to 'ArchTitus'. 
    Already tested in virtual box and it works flawlessly.
    Chose this one, because I know, that this is the go-to-theme for Chris Titus.

--- 3-post-setup.sh
+++ 3-post-setup.sh
@@ -17,7 +17,23 @@ GRUB EFI Bootloader Install & Check
 if [[ -d "/sys/firmware/efi" ]]; then
     grub-install --efi-directory=/boot ${DISK}
 fi
+
+echo -e "Installing CyberRe Grub theme..."
+THEME_DIR="/boot/grub/themes"
+THEME_NAME=CyberRe
+echo -e "Creating the theme directory..."
+mkdir -p "${THEME_DIR}/${THEME_NAME}"
+echo -e "Copying the theme..."
+cd ${HOME}/ArchTitus
+cp -a ${THEME_NAME}/* ${THEME_DIR}/${THEME_NAME}
+echo -e "Backing up Grub config..."
+cp -an /etc/default/grub /etc/default/grub.bak
+echo -e "Setting the theme as the default..."
+grep "GRUB_THEME=" /etc/default/grub 2>&1 >/dev/null && sed -i '/GRUB_THEME=/d' /etc/default/grub
+echo "GRUB_THEME=\"${THEME_DIR}/${THEME_NAME}/theme.txt\"" >> /etc/default/grub
+echo -e "Updating grub..."
 grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "All set!"
 
 echo -ne "
 -------------------------------------------------------------------------
