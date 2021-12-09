4645abd790053d4c65ac712684e91507f582a986
fenris <fabian.hanrath@posteo.de>
2021-12-09 22:35:42 +0100

    absolute paths

--- 2-user.sh
+++ 2-user.sh
@@ -27,7 +27,7 @@ git clone "https://github.com/ChrisTitusTech/zsh"
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerlevel10k
 ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 
-yay -S --noconfirm --needed - < /pkg-files/aur-pkgs.txt
+yay -S --noconfirm --needed - < ~/$SCRIPTHOME/pkg-files/aur-pkgs.txt
 
 export PATH=$PATH:~/.local/bin
 cp -r $HOME/$SCRIPTHOME/dotfiles/* $HOME/.config/
