763393367383e879c61a84d85e19568f5c5eb6a2
fenris <fabian.hanrath@posteo.de>
2021-12-09 23:02:54 +0100

    drivers for uhd gpu

--- 1-setup.sh
+++ 1-setup.sh
@@ -102,7 +102,9 @@ if lspci | grep -E "NVIDIA|GeForce"; then
 elif lspci | grep -E "Radeon"; then
     pacman -S xf86-video-amdgpu --noconfirm --needed
 elif lspci | grep -E "Integrated Graphics Controller"; then
-    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils --needed --noconfirm
+    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
+elif lspci | grep -E "Intel Corporation UHD"; then
+    pacman -S libva-intel-driver libvdpau-va-gl lib32-vulkan-intel vulkan-intel libva-intel-driver libva-utils lib32-mesa --needed --noconfirm
 fi
 echo -ne "
 -------------------------------------------------------------------------
