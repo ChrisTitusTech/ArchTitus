e392700214cceeaf53a414cdba0a34fd733a9b6a
BasedJam <96068706+BasedJam@users.noreply.github.com>
2021-12-15 23:59:37 +1000

    Update 2-user.sh
    Changes to PKGS list
    
    Added:
    - Spotify
    
    Removed
    - awesome-terminal-fonts - doesn't exist anymore
    - lightly-git - unused theme
    - lightlyshaders-git - unused theme
    
    Also added descriptions to existing packages so I know what they are without having to look. Should maintain this going forward.

--- 2-user.sh
+++ 2-user.sh
@@ -23,32 +23,29 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $HOME/powerleve
 ln -s "$HOME/zsh/.zshrc" $HOME/.zshrc
 
 PKGS=(
-'autojump'
-'awesome-terminal-fonts'
-'brave-bin' # Brave Browser
+'autojump' # A faster way to navigate your filesystem from the command line
 'dxvk-bin' # DXVK DirectX to Vulcan
 'github-desktop-bin' # Github Desktop sync
-'lightly-git'
-'lightlyshaders-git'
 'mangohud' # Gaming FPS Counter
-'mangohud-common'
-'nerd-fonts-fira-code'
-'nordic-darker-standard-buttons-theme'
-'nordic-darker-theme'
-'nordic-kde-git'
-'nordic-theme'
-'noto-fonts-emoji'
-'papirus-icon-theme'
-'plasma-pa'
+'mangohud-common' # Common files for mangohud and lib32-mangohud
+'nerd-fonts-fira-code' # It's literally a heap of fonts
+'nordic-darker-standard-buttons-theme' # Theme
+'nordic-darker-theme' # Theme
+'nordic-kde-git' # Theme
+'nordic-theme' # Theme
+'noto-fonts-emoji' # Theme
+'papirus-icon-theme' # Theme
+'plasma-pa' # Plasma applet for audio volume management using PulseAudio
 'ocs-url' # install packages from websites
-'sddm-nordic-theme-git'
-'snapper-gui-git'
-'ttf-droid'
-'ttf-hack'
+'sddm-nordic-theme-git' # Theme
+'snap-pac' # Pacman hooks that use snapper to create pre/post btrfs snapshots like openSUSE's YaST
+'snapper-gui-git' # Gui for snapper, a tool of managing snapshots of Btrfs subvolumes and LVM volumes
+'spotify' #It's spotify bro you know what this is
+'ttf-droid' # General-purpose fonts released by Google as part of Android
+'ttf-hack' # A hand groomed and optically balanced typeface based on Bitstream Vera Mono.
 'ttf-meslo' # Nerdfont package
-'ttf-roboto'
+'ttf-roboto' # Google's signature family of fonts
 'zoom' # video conferences
-'snap-pac'
 )
 
 for PKG in "${PKGS[@]}"; do
