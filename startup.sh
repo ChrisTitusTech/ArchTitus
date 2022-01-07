3c6c7e908df31fd00bf92d3f849f7b4396e5b251
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-01-07 13:06:44 +0100

    display size of the disk

--- startup.sh
+++ startup.sh
@@ -126,7 +126,7 @@ esac
 # selection for disk type
 diskpart () {
 # show disks present on system
-lsblk -n --output TYPE,KNAME | awk '$1=="disk"{print NR,"/dev/"$2}' # show disks with /dev/ prefix
+lsblk -n --output TYPE,KNAME,SIZE | awk '$1=="disk"{print NR,"/dev/"$2" - "$3}' # show disks with /dev/ prefix and size
 echo -ne "
 ------------------------------------------------------------------------
     THIS WILL FORMAT AND DELETE ALL DATA ON THE DISK             
