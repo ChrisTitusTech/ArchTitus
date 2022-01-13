30e582683999aa59f24ccba9245ef937aa4a00f6
Chris Titus <dfm.titus@gmail.com>
2022-01-13 12:40:31 -0600

    server fixes

--- archtitus.sh
+++ archtitus.sh
@@ -2,6 +2,7 @@
 
 # Find the name of the folder the scripts are in
 
+setfont ter-v22b
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 echo -ne "
 -------------------------------------------------------------------------
