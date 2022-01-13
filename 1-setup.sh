29bef1ddaba3555031d1451ed63ce7f8a199a97b
Chris Titus <dfm.titus@gmail.com>
2022-01-13 15:09:00 -0600

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -26,7 +26,7 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 pacman -S --noconfirm pacman-contrib curl
-pacman -S --noconfirm reflector rsync
+pacman -S --noconfirm reflector rsync grub btrfs-progs arch-install-scripts git
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
 nc=$(grep -c ^processor /proc/cpuinfo)
