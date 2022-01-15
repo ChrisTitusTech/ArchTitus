be84f86b5d7b898874648f2b611f913abeb8558e
vic1707 <28602203+vic1707@users.noreply.github.com>
2022-01-15 16:53:56 +0100

    fix timezone

--- 1-setup.sh
+++ 1-setup.sh
@@ -49,7 +49,7 @@ echo -ne "
 "
 sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 locale-gen
-timedatectl --no-ask-password set-timezone ${TIMEZONE}
+ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 timedatectl --no-ask-password set-ntp 1
 localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
 
