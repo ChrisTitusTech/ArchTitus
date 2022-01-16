d0af744e79a725c248afdea2f8243e0334cddef9
mfgbhatti <mfgbhatti@live.com>
2022-01-16 19:05:48 +0000

    added https protocol

--- 0-preinstall.sh
+++ 0-preinstall.sh
@@ -35,7 +35,8 @@ echo -ne "
                     Setting up $iso mirrors for faster downloads
 -------------------------------------------------------------------------
 "
-reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+# added https protocol for mirrors
+reflector -a 48 -c $iso -f 5 -l 20 -p https --sort rate --save /etc/pacman.d/mirrorlist
 mkdir /mnt &>/dev/null # Hiding error message if any
 echo -ne "
 -------------------------------------------------------------------------
@@ -145,6 +146,8 @@ echo -ne "
 "
 pacstrap /mnt base base-devel linux linux-firmware vim nano sudo archlinux-keyring wget libnewt --noconfirm --needed
 echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
+# check pacstrap installed or not
+
 cp -R ${SCRIPT_DIR} /mnt/root/ArchTitus
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 echo -ne "
