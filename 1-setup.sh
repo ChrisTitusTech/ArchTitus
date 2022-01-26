03e2e79c2e7c06bbc68c6f537241a94a909b471b
stojshic <stojshic@arch>
2022-01-26 20:41:36 +0100

    Cleaning up duplicate files from pacman-pkgs that were already installed

--- 1-setup.sh
+++ 1-setup.sh
@@ -73,15 +73,17 @@ echo -ne "
 "
 # sed $INSTALL_TYPE is using install type to check for MINIMAL installation, if it's true, stop
 # stop the script and move on, not installing any more packages below that line
-sed -n '/'$INSTALL_TYPE'/q;p' /root/ArchTitus/pkg-files/pacman-pkgs.txt | while read line
-do
-  if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
-    # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
-    continue
-  fi
-  echo "INSTALLING: ${line}"
-  sudo pacman -S --noconfirm --needed ${line}
-done
+if [[ ! $DESKTOP_ENV == server ]]; then
+  sed -n '/'$INSTALL_TYPE'/q;p' /root/ArchTitus/pkg-files/pacman-pkgs.txt | while read line
+  do
+    if [[ ${line} == '--END OF MINIMAL INSTALL--' ]]; then
+      # If selected installation type is FULL, skip the --END OF THE MINIMAL INSTALLATION-- line
+      continue
+    fi
+    echo "INSTALLING: ${line}"
+    sudo pacman -S --noconfirm --needed ${line}
+  done
+fi
 echo -ne "
 -------------------------------------------------------------------------
                     Installing Microcode
