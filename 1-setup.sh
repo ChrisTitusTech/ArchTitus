e497821b1f34e4f4caba5f8b8039d51ea9767016
TMC3 <tmcarter@ultrastat.com>
2022-02-09 11:15:18 -0500

    Update 1-setup.sh

--- 1-setup.sh
+++ 1-setup.sh
@@ -57,7 +57,7 @@ localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
 localectl --no-ask-password set-keymap ${KEYMAP}
 
 # Add sudo no password rights
-sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
 
 #Add parallel downloading
 sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
