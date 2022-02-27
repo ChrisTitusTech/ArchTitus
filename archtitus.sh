dee731352d5aec6a8e51610b17d3e1216341e039
godalming123 <r2hk9ahnf@relay.firefox.com>
2022-02-27 10:40:10 +0000

    move logs to logs folder

--- archtitus.sh
+++ archtitus.sh
@@ -19,15 +19,16 @@ echo -ne "
 -------------------------------------------------------------------------
                 Scripts are in directory named ArchTitus
 "
-    ( bash $SCRIPT_DIR/scripts/startup.sh )|& tee startup.log
-      source $CONFIGS_DIR/setup.conf
-    ( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee 0-preinstall.log
-    ( arch-chroot /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& tee 1-setup.log
-    if [[ ! $DESKTOP_ENV == server ]]; then
-      ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee 2-user.log
-    fi
-    ( arch-chroot /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee 3-post-setup.log
-    cp -v *.log /mnt/home/$USERNAME
+mkdir logs
+( bash $SCRIPT_DIR/scripts/startup.sh )|& tee logs/startup.log
+source $CONFIGS_DIR/setup.conf
+( bash $SCRIPT_DIR/scripts/0-preinstall.sh )|& tee logs/0-preinstall.log
+( arch-chroot /mnt $HOME/ArchTitus/scripts/1-setup.sh )|& logs/tee 1-setup.log
+if [[ ! $DESKTOP_ENV == server ]]; then
+  ( arch-chroot /mnt /usr/bin/runuser -u $USERNAME -- /home/$USERNAME/ArchTitus/scripts/2-user.sh )|& tee logs/2-user.log
+fi
+( arch-chroot /mnt $HOME/ArchTitus/scripts/3-post-setup.sh )|& tee logs/3-post-setup.log
+cp -v *.log /mnt/home/$USERNAME
 
 echo -ne "
 -------------------------------------------------------------------------
