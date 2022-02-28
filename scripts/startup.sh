719273b4c35b1c7557c99354c1d2577b00537af0
farhan <mfgbhatti@live.com>
2022-02-28 15:44:35 +0000

    background checks 2

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -34,8 +34,25 @@ docker_check() {
     fi
 }
 
+arch_check() {
+    if [[ ! -e /etc/arch-release ]]; then
+        echo -ne "ERROR! This script must be run in Arch Linux!\n"
+        exit 0
+    fi
+}
+
+pacman_check() {
+    if [[ -f /var/lib/pacman/db.lck ]]; then
+        echo "ERROR! Pacman is blocked."
+        echo -ne "If not running remove /var/lib/pacman/db.lck.\n"
+        exit 0
+    fi
+}
+
 background_checks() {
     root_check
+    arch_check
+    pacman_check
     docker_check
 }
 # Renders a text based list of options that can be selected by the
