9129d10631fcd6910ae80b9c6334c5fea92aac0b
Austin Horstman <khaneliman12@gmail.com>
2022-02-28 23:33:10 -0600

    Only install/theme plymouth on full install

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -120,24 +120,26 @@ cp -rfv ${SNAPPER_CONF_D} /etc/conf.d/
 
 fi
 
-echo -ne "
--------------------------------------------------------------------------
-              Enabling (and Theming) Plymouth Boot Splash
--------------------------------------------------------------------------
-"
-PLYMOUTH_THEMES_DIR="$HOME/ArchTitus/configs/usr/share/plymouth/themes"
-PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
-mkdir -p "/usr/share/plymouth/themes"
-echo 'Installing Plymouth theme...'
-cp -rf "${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME}" "/usr/share/plymouth/themes"
-if  [[ "${FS}" == "luks" ]]; then
-  sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
-  sed -i 's/HOOKS=(base udev \(.*block\) /&plymouth-/' /etc/mkinitcpio.conf # create plymouth-encrypt after block hook
-else
-  sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
+if [[ ${INSTALL_TYPE} == "FULL" ]]; then
+  echo -ne "
+  -------------------------------------------------------------------------
+                Enabling (and Theming) Plymouth Boot Splash
+  -------------------------------------------------------------------------
+  "
+  PLYMOUTH_THEMES_DIR="$HOME/ArchTitus/configs/usr/share/plymouth/themes"
+  PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
+  mkdir -p "/usr/share/plymouth/themes"
+  echo 'Installing Plymouth theme...'
+  cp -rf "${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME}" "/usr/share/plymouth/themes"
+  if  [[ "${FS}" == "luks" ]]; then
+    sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
+    sed -i 's/HOOKS=(base udev \(.*block\) /&plymouth-/' /etc/mkinitcpio.conf # create plymouth-encrypt after block hook
+  else
+    sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
+  fi
+  plymouth-set-default-theme -R arch-glow # sets the theme and runs mkinitcpio
+  echo 'Plymouth theme installed'
 fi
-plymouth-set-default-theme -R arch-glow # sets the theme and runs mkinitcpio
-echo 'Plymouth theme installed'
 
 echo -ne "
 -------------------------------------------------------------------------
