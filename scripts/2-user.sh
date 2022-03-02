3663f42d88d26145cbf88be3906248174b541a48
Austin Horstman <khaneliman12@gmail.com>
2022-03-02 08:58:35 -0600

    Fix Zshrc link

--- scripts/2-user.sh
+++ scripts/2-user.sh
@@ -17,11 +17,11 @@ Installing AUR Softwares
 source $HOME/ArchTitus/configs/setup.conf
 
   cd ~
-  mkdir "/home/$USERNAME/.cache"
-  touch "/home/$USERNAME/.cache/zshhistory"
+  mkdir "$HOME/.cache"
+  touch "$HOME/.cache/zshhistory"
   git clone "https://github.com/ChrisTitusTech/zsh"
   git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
-  ln -s "~/zsh/.zshrc" ~/.zshrc
+  ln -s "$HOME/zsh/.zshrc" ~/.zshrc
 
 sed -n '/'$INSTALL_TYPE'/q;p' ~/ArchTitus/pkg-files/${DESKTOP_ENV}.txt | while read line
 do
