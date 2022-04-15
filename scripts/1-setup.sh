d9ceaa35486d6f95064f95db659d35e1b9486261
Lukas Graz <gasfunken@googlemail.com>
2022-04-15 16:17:23 +0200

    fix locale and no gnome-extras

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -58,7 +58,9 @@ echo -ne "
 # 
 # sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
 sed -i 's/^#de_CH.UTF-8 UTF-8/de_CH.UTF-8 UTF-8/' /etc/locale.gen
-locale-gen
+cat "LANG=en_US.UTF-8" >> /etc/locale.conf
+cat "LC_TIME=de_CH.UTF-8" >> /etc/locale.conf
+
 timedatectl --no-ask-password set-timezone ${TIMEZONE}
 timedatectl --no-ask-password set-ntp 1
 # localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
@@ -67,6 +69,7 @@ ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 # Set keymaps
 localectl --no-ask-password set-keymap ${KEYMAP}
 localectl set-x11-keymap de acer_laptop nodeadkeys
+locale-gen
 
 # Add sudo no password rights
 sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
