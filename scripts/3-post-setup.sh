ee9b77af8d9d95d45e05e6b4deb92c71961dae3c
outerelocarlos <outerelocarlos@gmail.com>
2022-04-22 03:07:22 +0200

    Syntax error fix
    Fixed a syntax error in a conditional expression within the Plymouth section of the post-setup script (line 139).

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -136,7 +136,7 @@ PLYMOUTH_THEME="arch-glow" # can grab from config later if we allow selection
 mkdir -p /usr/share/plymouth/themes
 echo 'Installing Plymouth theme...'
 cp -rf ${PLYMOUTH_THEMES_DIR}/${PLYMOUTH_THEME} /usr/share/plymouth/themes
-if  [[ $FS == "luks"]]; then
+if  [[ ${FS} == "luks"]]; then
   sed -i 's/HOOKS=(base udev*/& plymouth/' /etc/mkinitcpio.conf # add plymouth after base udev
   sed -i 's/HOOKS=(base udev \(.*block\) /&plymouth-/' /etc/mkinitcpio.conf # create plymouth-encrypt after block hook
 else
