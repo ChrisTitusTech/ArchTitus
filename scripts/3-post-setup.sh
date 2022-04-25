32dec4603198aa8aa9163182bf567dde3cbf5ea8
Lukas Graz <gasfunken@googlemail.com>
2022-04-25 16:55:45 +0200

    .

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -154,8 +154,8 @@ sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: A
 sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
 sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
-rm -r $HOME/ArchTitus
-rm -r /home/$USERNAME/ArchTitus
+# rm -r $HOME/ArchTitus
+# rm -r /home/$USERNAME/ArchTitus
 
 # Replace in the same state
 cd $pwd
