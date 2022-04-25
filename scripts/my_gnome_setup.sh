723ca87a75813e443992de3381a7ee67605a33d8
Lukas Graz <gasfunken@googlemail.com>
2022-04-25 23:17:47 +0200

    .

--- scripts/my_gnome_setup.sh
+++ scripts/my_gnome_setup.sh
@@ -35,15 +35,25 @@ printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 3088 # Extension li
 printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 906 # Sound output chooser
 
 killall -HUP gnome-shell # kill (hopfully restart) gnome-shell
-enable-extension () {
+init-extension () {
+    # enables extensions and links to gsettings
     gnome-extensions enable $1 || echo "could not enable $1"
+    files=`ls ~/.local/share/gnome-shell/extensions/$1/schemas/`
+    for file in $files; do
+        if [[ $file == *.xml ]]; then
+            echo "$file"
+            sudo cp ~/.local/share/gnome-shell/extensions/extname/schemas/$file /usr/share/glib-2.0/schemas/
+        fi
+    done;
 }
-enable-extension clipboard-indicator@tudmotu.com
-enable-extension impatience@gfxmonk.net
-enable-extension material-shell@papyelgringo
-enable-extension Vitals@CoreCoding.com
-enable-extension unite@hardpixel.eu
-enable-extension extension-list@tu.berry
-enable-extension sound-output-device-chooser@kgshank.net
-enable-extension gnome-shell-screenshot@ttll.de
+init-extension clipboard-indicator@tudmotu.com
+init-extension impatience@gfxmonk.net
+init-extension material-shell@papyelgringo
+init-extension Vitals@CoreCoding.com
+init-extension unite@hardpixel.eu
+init-extension extension-list@tu.berry
+init-extension sound-output-device-chooser@kgshank.net
+init-extension gnome-shell-screenshot@ttll.de
 
+# compile schemas (which are linked)
+sudo glib-compile-schemas /usr/share/glib-2.0/schemas/
