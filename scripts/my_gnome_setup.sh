9ad48fa162404666c96cea7d8a50742d3ed68945
Lukas Graz <gasfunken@googlemail.com>
2022-04-25 16:31:42 +0200

    .

--- scripts/my_gnome_setup.sh
+++ scripts/my_gnome_setup.sh
@@ -5,14 +5,16 @@ git config --global user.email "lukasgraz@gmail.com"
 git config --global user.name "LukasGraz"
 
 # download this repo
+cd Desktop
 git clone https://github.com/Greeenstone/ArchTitus
+cd ..
 
 # disable Wayland
-sed -i 's/^#WaylandEnable=false/WaylandEnable=false/' /etc/gdm/custom.conf
+sudo sed -i 's/^#WaylandEnable=false/WaylandEnable=false/' /etc/gdm/custom.conf
 
 # fix KEYMAP
-gsettings reset org.gnome.desktop.input-sources sources
-gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'de+nodeadkeys')]"
+sudo gsettings reset org.gnome.desktop.input-sources sources
+sudo gsettings set org.gnome.desktop.input-sources sources "[('xkb', 'de+nodeadkeys')]"
 
 # SHORTCUTS
 # TEMP="/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings"
@@ -28,15 +30,15 @@ temp=`gnome-shell --version`
 GNOME_VERSION=${temp:12:2} && echo "gnome version: $GNOME_VERSION"
 
 # get extension installer:
-yay -S gnome-shell-extension-installer
+yay -S --noconfirm --needed gnome-shell-extension-installer
 
-printf $GNOME_VERSION | gnome-shell-extension-installer 779 # clipboard-indicator
-printf $GNOME_VERSION | gnome-shell-extension-installer 277 # impatience
-printf $GNOME_VERSION | gnome-shell-extension-installer 3357 # material shell
-printf $GNOME_VERSION | gnome-shell-extension-installer 1460 # vitals (cpu/ram/storage/..)
-printf $GNOME_VERSION | gnome-shell-extension-installer 1287 # unite (remove window top panel)
-printf $GNOME_VERSION | gnome-shell-extension-installer 1112 # screenshot tool
-printf $GNOME_VERSION | gnome-shell-extension-installer 3088 # Extension list
-printf $GNOME_VERSION | gnome-shell-extension-installer 906 # Sound output chooser
+printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 779 # clipboard-indicator
+printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 277 # impatience
+printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 3357 # material shell
+printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 1460 # vitals (cpu/ram/storage/..)
+printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 1287 # unite (remove window top panel)
+printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 1112 # screenshot tool
+printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 3088 # Extension list
+printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 906 # Sound output chooser
 
 killall -HUP gnome-shell # kill (hopfully restart) gnome-shell
