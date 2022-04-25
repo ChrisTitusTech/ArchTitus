f0bcb96d97336a1d70ac99015d4a53c77c4ca130
i-c-u-p <96894903+i-c-u-p@users.noreply.github.com>
2022-04-25 07:30:04 +0000

    fix invalid sed expressions

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -46,7 +46,7 @@ echo "
 -------------------------------------------------------------------------
                     Setup Language to US and set locale  
 -------------------------------------------------------------------------"
-sed -i 's/en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen
+sed -i '/en_US.UTF-8 UTF-8/s/^#//' /etc/locale.gen
 locale-gen
 timedatectl --no-ask-password set-timezone ${TIMEZONE}
 timedatectl --no-ask-password set-ntp 1
@@ -56,11 +56,11 @@ ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 localectl --no-ask-password set-keymap ${KEYMAP}
 
 # Add sudo no password rights
-sed -i 's/%wheel ALL=(ALL) NOPASSWD: ALL/s/^# //' /etc/sudoers
-sed -i 's/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/^# //' /etc/sudoers
+sed -i '/%wheel ALL=(ALL) NOPASSWD: ALL/s/^# //' /etc/sudoers
+sed -i '/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/s/^# //' /etc/sudoers
 
 #Add parallel downloading
-sed -i 's/ParallelDownloads/s/^#//' /etc/pacman.conf
+sed -i '/ParallelDownloads/s/^#//' /etc/pacman.conf
 
 #Enable multilib
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
