eae1033f79912eb74e3a20708a216a4802ff1774
Lukas Graz <gasfunken@googlemail.com>
2022-04-25 16:41:04 +0200

    .

--- scripts/my_gnome_setup.sh
+++ scripts/my_gnome_setup.sh
@@ -32,13 +32,13 @@ GNOME_VERSION=${temp:12:2} && echo "gnome version: $GNOME_VERSION"
 # get extension installer:
 yay -S --noconfirm --needed gnome-shell-extension-installer
 
-printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 779 # clipboard-indicator
-printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 277 # impatience
-printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 3357 # material shell
-printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 1460 # vitals (cpu/ram/storage/..)
-printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 1287 # unite (remove window top panel)
-printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 1112 # screenshot tool
-printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 3088 # Extension list
-printf "$GNOME_VERSION\nq" | gnome-shell-extension-installer 906 # Sound output chooser
+printf "$GNOME_VERSION\nq" | sudo gnome-shell-extension-installer 779 # clipboard-indicator
+printf "$GNOME_VERSION\nq" | sudo gnome-shell-extension-installer 277 # impatience
+printf "$GNOME_VERSION\nq" | sudo gnome-shell-extension-installer 3357 # material shell
+printf "$GNOME_VERSION\nq" | sudo gnome-shell-extension-installer 1460 # vitals (cpu/ram/storage/..)
+printf "$GNOME_VERSION\nq" | sudo gnome-shell-extension-installer 1287 # unite (remove window top panel)
+printf "$GNOME_VERSION\nq" | sudo gnome-shell-extension-installer 1112 # screenshot tool
+printf "$GNOME_VERSION\nq" | sudo gnome-shell-extension-installer 3088 # Extension list
+printf "$GNOME_VERSION\nq" | sudo gnome-shell-extension-installer 906 # Sound output chooser
 
 killall -HUP gnome-shell # kill (hopfully restart) gnome-shell
