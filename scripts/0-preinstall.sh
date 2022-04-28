e273a05a54c64cfb889e8c8d9de98e01db552c54
i-c-u-p <96894903+i-c-u-p@users.noreply.github.com>
2022-04-28 23:36:12 +0000

    change paralleldownloads uncommenting sed
    not sure why i put a . instead of a #.

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -30,7 +30,7 @@ timedatectl set-ntp true
 pacman -S --noconfirm archlinux-keyring #update keyrings to latest to prevent packages failing to install
 pacman -S --noconfirm --needed pacman-contrib terminus-font
 setfont ter-v22b
-sed -i '/^#ParallelDownloads/s/^.//' /etc/pacman.conf
+sed -i '/^#ParallelDownloads/s/^#//' /etc/pacman.conf
 pacman -S --noconfirm --needed reflector rsync grub
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 echo "
